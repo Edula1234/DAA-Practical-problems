e81c1a923e6fcde0cb6bd9bb4a42ac0d33508038
Pranith <130446457+Edula1234@users.noreply.github.com>
2024-03-31 15:39:21 +0530

    Update 6.Prime Number Not Using Recursion.cpp

--- 6.Prime Number Not Using Recursion.cpp
+++ 6.Prime Number Not Using Recursion.cpp
@@ -7,16 +7,13 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (num <= 1) 
-	{
+    if (num <= 1) {
         printf("%d is not a prime number.\n", num);
         return 0;
     }
 
-    for (i = 2; i <= num / 2; ++i) 
-	{
-        if (num % i == 0) 
-		{
+    for (i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) {
             flag = 1;
             break;
         }
